0638cfb58d1ba3b50d1d7b9d87698e0628561f5e
Dmitri Melikyan <dmitri@rapidlogix.com>
2012-11-07 10:47:27 +0100

    0.6.0 release

---
+++ test/probes/mysql2.js
@@ -0,0 +1,42 @@
+
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+  user: 'root',
+  password: ''
+});
+
+module.exports = function(cb) {
+  client.query('create database test_db', function(err) {
+    if (err) {
+      console.error(err);
+    }
+
+    client.query('use test_db', function(err) {
+      client.query('create temporary table test_table (id int(11) auto_increment, test_field text, created datetime, primary key(id))', function(err) {
+        if(err) {
+          console.error(err);
+        }
+  
+        client.query('insert into test_table set test_field=?, created=?', ['data', '2012-03-25 10:00:00'], function(err) {
+          if(err) {
+            console.error(err);
+            cb();
+            return;
+          }
+
+          client.query('select * from test_table', function(err) {
+            if(err) {
+              console.error(err);
+              cb();
+              return;
+            }
+  
+            cb();
+          });
+        });
+      });
+    });
+  });
+};
+
